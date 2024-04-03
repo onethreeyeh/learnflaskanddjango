670b7ffb3f2b86b9a3e346d787749641f9ff8a38
onethreeyeh <onethreeyeh@outlook.com>
2024-04-03 15:53:24 +0800

    2024/04/03 15:52:55

--- web.py
+++ web.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/whoami")
+def whoami():
+	return render_template("whoami.html")
 
 if __name__ == '__main__':
 	app.run()	
